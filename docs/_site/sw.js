aaaebbcf9f9520b76d5d23872066002a70217d23
Poojan Shah <poojanshah@Poojans-MacBook-Pro.local>
2025-02-08 21:50:09 -0500

    commit

--- docs/_site/sw.js
+++ docs/_site/sw.js
@@ -4,7 +4,7 @@ self.addEventListener("install", function(e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll([
-        "/css/pixyll.css?202502061652",
+        "/css/pixyll.css?202502082149",
         "/"
       ]);
     })
